1c675465ae2d6168a213571ed22a810609b6610c
sstsuji <tsuji.satoki@fujitsu.com>
2024-08-21 23:25:12 +0900

    Add scripts in run/ for reproducing experimental results in the paper

---
+++ run/bulk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+binary=${1}
+
+for i in {15..27}
+do
+    OMP_NUM_THREADS=$(nproc) ../bin/${binary} host single run ${i} 24 40.0
+    #OMP_NUM_THREADS=${nproc} ../bin/${binary} host incremental run ${i} 24 40.0
+    #../bin/${binary} device single run ${i} 24 40.0
+    #../bin/${binary} device incremental run ${i} 24 40.0
+done
---
+++ run/taylor.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd ..
+
+binary=${1}
+
+xi_intervals=()
+num_xis=()
+k_maxes=(8, 7, 6, 5, 5, 4, 4, 3)
+for i in {2..9}
+do
+    xi_interval=$(echo "scale=10; 2^(-${i})" | bc)
+    num_xi=$(echo "scale=0; 32/${xi_interval}" | bc)
+    xi_intervals+=($xi_interval)
+    num_xis+=($num_xi)
+done
+#echo "xi intervals: ${xi_intervals[@]}"
+#echo "#xis: ${num_xis[@]}"
+
+for j in "${!k_maxes[@]}"
+do
+    make -s -B BIN=${binary} \
+         XI_INTERVAL=${xi_intervals[${j}]} \
+         NUM_XI=${num_xis[${j}]} \
+         K_MAX=${k_maxes[${j}]}
+    
+    ./bin/${binary} device single test 15 24 7.5
+    #./bin/${binary} device incremental run 22 24 7.5
+done
+
+cd -
