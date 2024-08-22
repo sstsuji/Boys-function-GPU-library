e1a46fe2472fe1ab1bb70a14110a062187772b93
sstsuji <tsuji.satoki@fujitsu.com>
2024-08-22 11:29:04 +0900

    Update README.md

--- run/bulk.sh
+++ run/bulk.sh
@@ -13,8 +13,8 @@ binary=${1}
 
 for i in {15..27}
 do
-    OMP_NUM_THREADS=$(nproc) ../bin/${binary} host single run ${i} 24 40.0
-    #OMP_NUM_THREADS=${nproc} ../bin/${binary} host incremental run ${i} 24 40.0
+    #OMP_NUM_THREADS=$(nproc) ../bin/${binary} host single run ${i} 24 40.0
+    #OMP_NUM_THREADS=$(nproc) ../bin/${binary} host incremental run ${i} 24 40.0
     #../bin/${binary} device single run ${i} 24 40.0
-    #../bin/${binary} device incremental run ${i} 24 40.0
+    ../bin/${binary} device incremental run ${i} 24 40.0
 done
